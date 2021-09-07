658653b64b6beb5c8a425dd96a584d6199ae5b0d
aayoungfish <zhuruolan1@163.com>
2021-09-07 10:43:28 +0800

    addfile

---
+++ filterdb.py
@@ -0,0 +1,47 @@
+#usage:-i -r ><out_dir/out_file>
+
+import argparse
+import os
+import re
+
+
+def filterdb(input_file,ref_file):
+    with open (input_file) as f_in:
+        txt = f_in.read()
+        m = re.findall(r'STRG\.\d+\.\d+',txt)
+        n = set(m)
+        l = list(n)
+#        print (l)
+	
+#    out_dir = os.getcwd()
+#    if not os.path.exists(out_dir):
+#        os.mkdir(out_dir)	
+#    f_out = open('filteraa.fa', mode ='w', encoding ='utf-8')
+	
+    seq = {}
+    sequence = ""
+    with open(ref_file) as f_ref:
+        for line in f_ref:
+            if line.startswith(">"):
+                name = line[1:].rstrip()
+                seq[name] = ""
+                continue
+            seq[name] += line.rstrip().upper()
+        for k,v in seq.items():
+            if k in l:
+                continue
+            print('>' + k + '\n' + v + '\n')
+#            f_out.write('>' + k + '\n' + v + '\n')
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="This is the script for fa_file to filter \
+                                                  which gffcompare need")
+    parser.add_argument('-i', '--input_file', required=True,
+                        help='<filepath>  The orf file which have been predited using getorf')
+    parser.add_argument('-r', '--ref_file', required=True,
+                        help='<filepath>  The original fa_file U choose to filter')
+#    parser.add_argument('-o', '--output_dir', required=True,
+#                        help='<dirpath>  the directory for results')
+    args = parser.parse_args()
+    filterdb(args.input_file,args.ref_file)
+	
